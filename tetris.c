49b101b229c0f2152ba42e8164821013c0e6b90f
Hvozd <mq-9@outlook.com>
2016-10-14 17:54:59 +0200

    first commit

--- tetris.c
+++ tetris.c
@@ -1,90 +1,145 @@
-						
-						//ALGORITHM 1
-
-
 #include "karel.h"
 
+void defineLeftSide();
+void defineShape();
+void climbStairs();
+void turnBack();
+void turnRight();
+void moveToItem();
+void item1();
+
 int main()
-{ 	turnOn("tetris.kw");
-
-	while( rightIsBlocked() ){
-		while( frontIsClear() ){
-			movek();
-			if ( frontIsBlocked() )
-			{	
-				if ( noBeepersPresent() )
-				{
-					putBeeper();
-				}
-				turnLeft();
-			}
-		}
-
-	}
+{ 	turnOn("tetris1.kw");
 	
-
+	setStepDelay(100);
+	moveToItem();
+	defineShape();
 	turnOff();
 return 0;
 }
+			//Define functions
 
 
-						//ALGORITHM 2
+			//Define Shape of Item
+void defineShape() {
+	if ( frontIsBlocked() )
+	{
+		turnRight();
+		defineLeftSide();
+	}
+}
+
+			//Define Center Side Of Item
+void defineCenterSide() {
+	if (!frontIsBlocked())
+			{
+				movek();
+				putBeeper();
+			}
+	else {
+		turnRight();
+		movek();
+		turnLeft();
+		movek();
+		loop(2){
+			putBeeper();
+		}
+	}		
+}
+
+			//Define Left Side Of Item
+void defineLeftSide() {
+	if ( leftIsBlocked() )
+		{
+			movek();
+			if ( leftIsBlocked() )
+			{
+				movek();
+				turnLeft();
+				movek();
+				loop(2){
+					putBeeper();
+				}
+			}
+			else {
+				turnLeft();
+				movek();
+				putBeeper();
+			}
+		}
+}
 
 
+			//Turn Right
+void turnRight() {
+	loop(3) {
+		turnLeft();
+	}
+}
 
-#include "karel.h"
 
-// int main()
-// { 	turnOn("marker3.kw");
-
-// 	while( beepersInBad() > 4 ){
-// 		while( frontIsClear() ){
-// 			movek();
-// 			if ( frontIsBlocked() )
-// 			{	
-// 				if ( noBeepersPresent() )
-// 				{
-// 					putBeeper();
-// 				}
-// 				turnLeft();
-// 			}
-// 		}
-
-// 	}
+			//Move Back
+void moveBack() {
+	loop(2) {
+		turnLeft();
+	}
+	movek();
+}
 	
+			//Turn Back
+void turnBack() {
+	loop(2) {
+		turnLeft();
+	}
+}
 
-// 	turnOff();
-// return 0;
-// }
+			//Climb Stairs
+void climbStairs() {
+	if ( rightIsBlocked() )
+	{
+		turnBack();
+	}
+}
+
+			//MovekToItem
+void moveToItem() {
+	while( frontIsClear() ){
+		movek();		
+	}
+	turnRight();
+	while( frontIsClear() ){
+		movek();		
+	}
+}
+
+			//Define Type Of Item 1
+void item1() {
+	if (frontIsBlocked() && rightIsClear()) {
+		turnRight();
+		movek();
+		if (rightIsClear()){
+			turnLeft();
+			movek();
+		}
+		while(leftIsBlocked()){
+			movek();
+		}
+	}
+}
+
+			//Search place for Item1
+void placeForItem1() {
+	moveBack();
+	movek();
+	turnLeft();
+	while ( frontIsClear() && rightIsClear() ) {
+		movek();
+		if ( rightIsBlocked() )
+		{
+			moveBack();
+		}
+
+	}
 
 
-
-						//ALGORIGHTM 3
-
-
-
-// #include "karel.h"
-
-// int main()
-// { 	turnOn("marker2.kw");
-
-// int count = 0;
-
-// 			while( count != 4 ){
-// 				movek();
-// 				if ( frontIsBlocked() )
-// 				{	
-// 					if (!beepersPresent())
-// 					{
-// 						putBeeper();
-// 						count++;
-// 					}
-// 					else 
-// 					count++;	
-// 					turnLeft();
-// 				}
-// 			}
-
-// 	turnOff();
-// return 0;
-// }
+}
